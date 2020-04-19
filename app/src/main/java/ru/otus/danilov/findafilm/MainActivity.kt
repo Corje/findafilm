db4ba58970315dcc9801efc9d4dec04fd214e54a
Alexander Danilov <alexander.danilov@team.wrike.com>
2020-04-19 17:33:34 +0300

    7 and some trnslations

--- app/src/main/java/ru/otus/danilov/findafilm/MainActivity.kt
+++ app/src/main/java/ru/otus/danilov/findafilm/MainActivity.kt
@@ -2,12 +2,18 @@ package ru.otus.danilov.findafilm
 
 import android.app.Activity
 import android.content.Intent
+import android.content.res.Configuration
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
 import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity(), BackDialogFragment.BackDialogListener {
@@ -65,6 +71,36 @@ class MainActivity : AppCompatActivity(), BackDialogFragment.BackDialogListener 
             }
             filmViewModel.selectedViewIndex?.let { textViewsList[it].setBackgroundResource(R.color.selected_color) }
         }
+
+        //AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        val menuItem = menu.findItem(R.id.action_night_mode)
+        val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        if (currentNightMode == Configuration.UI_MODE_NIGHT_YES) {
+            menuItem.isChecked = true
+        }
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_night_mode -> {
+                if (item.isChecked) {
+                    item.isChecked = false
+                    AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+                    true
+                } else {
+                    item.isChecked = true
+                    AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+                    true
+                }
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -100,8 +136,13 @@ class MainActivity : AppCompatActivity(), BackDialogFragment.BackDialogListener 
 
     companion object {
         val SNATCH = Film(R.drawable.snatch, R.string.snatch_description, R.string.snatch)
-        val LOCK_STOCK = Film(R.drawable.lock_stock_and_two_smoking_barrels, R.string.lock_stock_description, R.string.lock_stock_and_two_smoking_barrels)
-        val ROCKNROLLA = Film(R.drawable.rocknrolla, R.string.rocknrolla_description, R.string.rocknrolla)
+        val LOCK_STOCK = Film(
+            R.drawable.lock_stock_and_two_smoking_barrels,
+            R.string.lock_stock_description,
+            R.string.lock_stock_and_two_smoking_barrels
+        )
+        val ROCKNROLLA =
+            Film(R.drawable.rocknrolla, R.string.rocknrolla_description, R.string.rocknrolla)
 
         const val MY_TAG = "myTag"
         const val SELECTED_VIEW_INDEX = "selectedViewIndex"
